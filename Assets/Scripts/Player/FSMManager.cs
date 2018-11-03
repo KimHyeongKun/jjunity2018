da5cfdb5158e975b69ca8349e3d9ae1859721dc4
Dustin Lee <guru@dustinlee.me>
2018-11-03 15:47:55 +0900

    Add files via upload
    Marker

--- Assets/Scripts/Player/FSMManager.cs
+++ Assets/Scripts/Player/FSMManager.cs
@@ -14,17 +14,15 @@ public class FSMManager : MonoBehaviour {
 
     public PlayerState currentState;
     public PlayerState startState;
+    public Transform marker;
 
-    Dictionary<PlayerState, PlayerFSMState> states
-        = new Dictionary<PlayerState, PlayerFSMState>();
+    Dictionary<PlayerState, PlayerFSMState> states = new Dictionary<PlayerState, PlayerFSMState>();
 
     private void Awake()
     {
-        states.Add(
-            PlayerState.IDLE, 
-            GetComponent<PlayerIDLE>()
-        );
+        marker = GameObject.FindGameObjectWithTag("Marker").transform;
 
+        states.Add(PlayerState.IDLE,GetComponent<PlayerIDLE>());
         states.Add(PlayerState.RUN, GetComponent<PlayerRUN>());
         states.Add(PlayerState.CHASE, GetComponent<PlayerCHASE>());
         states.Add(PlayerState.ATTACK, GetComponent<PlayerATTACK>());
@@ -46,7 +44,7 @@ public class FSMManager : MonoBehaviour {
             RaycastHit hit;
             if(Physics.Raycast(r, out hit, 1000))
             {
-                Debug.Log(hit.point);
+                marker.position = hit.point;
             }
         }
 	}
