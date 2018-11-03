5a44aad114b86c77c2822cdfe7e3ba3acfa0286f
Dustin Lee <guru@dustinlee.me>
2018-11-03 13:13:10 +0900

    Add files via upload
    Step1

---
+++ Assets/Scripts/Player/FSMManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    IDLE = 0,
+    RUN,
+    CHASE,
+    ATTACK 
+}
+
+public class FSMManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
